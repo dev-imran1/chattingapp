913b21c9c442543919dfbf7abfd93abc03562ed2
dev-imran1 <freealltime247@gmail.com>
2024-01-19 18:42:04 +0600

    registration validation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <>
     <div>Homee</div>
-    
-    <div>Homee</div>
-    </>
   )
 }
 
--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -7,40 +7,94 @@ import CustomButton from '../../components/CustomButton';
 import AuthNavigate from '../../components/AuthNavigate';
 import ReginImg from '../../assets/images/regimg.png';
 import Image from '../../utilities/Image';
+import '../registration/registration.css'
+import Alert from '@mui/material/Alert';
+
 
 const Registration = () => {
-  let [singnupData, setSignupData] = useState({
-    name : "",
-    fullname : "",
-    password : ""
+
+  let [singupData, setSignupdata] = useState({
+    email : '',
+    fullname: '',
+    password : ''
+  });
+  let [error, setError] = useState({
+    email : '',
+    fullname: '',
+    password : ''
   })
 
+
+
   let handelFrom = (e) =>{
-    let {a, b} =e.target;
     let {name, value} = e.target;
-    console.log(value);
-    setSignupData({
-      ...singnupData,
-      [name] : value
+    setSignupdata({
+      ...singupData,
+      [name]:value
     })
   }
 
+  let emailRegex =/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  
   let handelSubmit = () =>{
-    console.log("ok");
+    if(!singupData.email){
+      setError({email: "Type Your email"});
+    }
+    else if(!singupData.email.match(emailRegex)){
+      setError({email: "wrong email"});
+    }
+    else if(!singupData.fullname){
+      setError({fullname: "fullName error"});
+    }
+    else if(!singupData.password){     
+      setError({password: "password error"});
+    }
+    else{
+      setError({
+        email : '',
+        fullname: '',
+        password : ''
+      })
+      console.log('done');
+    }
   }
   return (
     <div>
-      <h1>hello</h1>
-      <h1>hello</h1>
     <Grid container>
       <Grid item xs={6}>
         <div className='loginbox'>
           <div className='loginbox__inner'>
             <SectionHeading style='auth_heading' text='Get started with easily register'/>
             <div className='from_main'>
-             <Input onChange={handelFrom} name="email" variant="outlined" labelText="Email Add" style="login__input-field" type="email"/>
-             <Input onChange={handelFrom} name="fullname" variant="outlined" labelText="Full Name" style="login__input-field" type="text"/>
-             <Input onChange={handelFrom} name="password" variant="outlined" labelText="Type Password" style="login__input-field" type="password"/>
+              <div>
+                <Input onChange={handelFrom} name="email" variant="outlined" labelText="Email Add" style="login__input-field" type="email"/>
+                {error.email 
+                && 
+                // <p className='error'>{error.email}</p>
+                <Alert variant="filled" severity="error">
+                  {error.email}
+                </Alert>
+                }
+              </div>
+              <div>
+                <Input onChange={handelFrom} name="fullname" variant="outlined" labelText="Full Name" style="login__input-field" type="text"/>
+                {error.fullname 
+                && 
+                <Alert variant="filled" severity="error">
+                  {error.fullname}
+                </Alert>
+                }
+              </div>
+              <div>
+                <Input onChange={handelFrom} name="password" variant="outlined" labelText="Type Password" style="login__input-field" type="password"/>
+                {error.password 
+                &&
+                <Alert variant="filled" severity="error">
+                  {error.password}
+                </Alert>
+                }
+              </div>
              <CustomButton onClick={handelSubmit} styleing="loginBtn" variant="contained" btnText="Login to Continue"/>
             </div>
             <AuthNavigate style="login__auth" link="/" text="Already have an account ?" linkText="Sign in"/>
