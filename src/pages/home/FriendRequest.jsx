fe4e4b3bb7709e4196209614788628a66129ed59
dev-imran1 <freealltime247@gmail.com>
2024-02-10 03:15:47 +0600

    userlist, sentfriend request

--- src/pages/home/FriendRequest.jsx
+++ src/pages/home/FriendRequest.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import GroupCard from '../../components/home/GroupCard'
-import Image from '../../utilities/Image'
-
+import React, { useEffect, useState } from 'react'
+import GroupCard from '../../components/home/GroupCard';
+import Image from '../../utilities/Image';
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import { useSelector, useDispatch } from 'react-redux'
 
 const FriendRequest = () => {
+  const db = getDatabase();
+  const userCountRef = ref(db, 'users');
+  const [fRequest, setFRequest] = useState();
+  const dispatch = useDispatch();
+  const data = useSelector((state)=> state.loginuserdata.value);
+
+  useEffect(()=>{
+    const fRequestRef = ref(db, 'frequest');
+    onValue(fRequestRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach((item)=>{
+        if(data.uid == item.val().reciverid){
+          arr.push({...item.val(), id:item.key});
+          setFRequest(arr)
+        }
+        console.log(data.uid == item.val().reciverid)
+      })
+    });
+  },[])
+
   return (
     <>
         <GroupCard cardTitle="Friend Request" >
         <div className='usermainbox'>
-            {[0,1,2,3,4,5,6,7,8].map((item,index)=>(
+            {fRequest && fRequest.length > 0 
+            ?
+            fRequest.map((item,index)=>(
                 <div key={index} className='useritem'>
                     <div className='userimgbox'>
-                    <Image source="/" alt="image"/>
+                    <Image source={fRequest.profileimg} alt="image"/>
                     </div>
                     <div className='user_info-box'>
                     <div>
-                        <h3>siam talukder</h3>
+                        <h3>{fRequest.username}</h3>
                         <p>backend developer</p>
                     </div>
                     <div>
@@ -25,7 +48,10 @@ const FriendRequest = () => {
                     </div>
                     </div>
                 </div>
-            ))}
+            ))
+          :
+          <h2>No friend request</h2>
+          }
         </div>
       </GroupCard>
     
--- src/pages/home/UserList.jsx
+++ src/pages/home/UserList.jsx
@@ -1,32 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GroupCard from '../../components/home/GroupCard'
 import Image from '../../utilities/Image'
 import { FaPlus } from 'react-icons/fa6'
-import './home.css'
+import './home.css';
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import { useSelector, useDispatch } from 'react-redux'
+
 
 const UserList = () => {
+
+  const db = getDatabase();
+  const userCountRef = ref(db, 'users');
+  const [userLists, setUserList] = useState();
+  const dispatch = useDispatch();
+  const data = useSelector((state)=> state.loginuserdata.value)
+
+
+  // console.log(data.uid)
+    
+
+  useEffect(()=>{
+    const userRef = ref(db, 'users');
+    onValue(userRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach((item)=>{
+        if(data.uid != item.key){
+          arr.push({...item.val(), id:item.key});
+          setUserList(arr)
+        }
+        // console.log(item.key)
+      })
+    });
+  },[])
+  // console.log(data)
+  let handelFRequest = (frequestinfo) =>{
+    set(ref(db, 'frequest'), {
+      senderid: data.uid,
+      senderName: data.displayName,
+      senderimg: data.photoURL,
+      senderemail: data.email,
+      reciverid: frequestinfo.id,
+      reciverName: frequestinfo.username,
+      reciverimg: frequestinfo.profileimg
+    });
+  }
   return (
     <>
           <GroupCard cardTitle="User List" >
         <div className='usermainbox'>
-          {[0,1,2,3,4,5,6,7].map((item,index)=>(
-          <div className='useritem' key={index}>
+          {userLists && userLists.length > 0 
+          ?
+          userLists.map((item,index)=>(
+            <div className='useritem' key={index}>
             <div className='userimgbox'>
-              <Image source="/" alt="image"/>
+              <Image source={item.profileimg} alt="image"/>
             </div>
             <div className='user_info-box'>
               <div>
-                <h3>siam talukder</h3>
+                <h3>{item.username}</h3>
                 <p>backend developer</p>
               </div>
               <div>
-                <button className='addbtn'>
+                <button className='addbtn' onClick={()=>handelFRequest(item)}>
                   <FaPlus />
                 </button>
               </div>
             </div>
           </div>
-          ))}
+          ))
+          :
+          <h2>not found</h2>
+          }
         </div>
       </GroupCard>
     </>
